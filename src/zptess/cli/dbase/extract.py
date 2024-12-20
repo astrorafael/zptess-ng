9639945a5de3846931321fbd5ef0c7bb43e237ba
astrorafael <astrorafael@gmail.com>
2024-12-20 16:01:29 +0100

    initial version

---
+++ src/zptess/cli/dbase/extract.py
@@ -0,0 +1,232 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2024 Rafael Gonzalez.
+#
+# See the LICENSE file for details
+# ----------------------------------------------------------------------
+
+# --------------------
+# System wide imports
+# -------------------
+
+import os
+
+import csv
+import logging
+
+from argparse import ArgumentParser, Namespace
+from typing import Sequence, Iterable
+
+# -------------------
+# Third party imports
+# -------------------
+
+from lica.cli import execute
+from lica.validators import vdir
+from lica.sqlite import open_database
+
+# --------------
+# local imports
+# -------------
+
+from ... import __version__
+
+# ----------------
+# Module constants
+# ----------------
+
+DESCRIPTION = "TESS-W Calibration Database data extraction tool"
+
+CONFIG_H = ("section", "prop", "value")
+BATCH_H = ("begin_tstamp", "end_tstamp", "email_sent", "calibrations", "comment")
+PHOTOMETER_H = ("name", "mac", "sensor", "model", "firmware", "filter", "plug", "box", "collector")
+SUMMARY_H = (
+    "name",
+    "mac",
+    "session",
+    "role",
+    "calibration",
+    "calversion",
+    "author",
+    "nrounds",
+    "zp_offset",
+    "upd_flag",
+    "prev_zp",
+    "zero_point",
+    "zero_point_method",
+    "freq",
+    "freq_method",
+    "mag",
+    "comment",
+)
+ROUNDS_H = (
+    "session",
+    "round",
+    "role",
+    "begin_tstamp",
+    "end_tstamp",
+    "central",
+    "freq",
+    "stddev",
+    "mag",
+    "zp_fict",
+    "zero_point",
+    "nsamples",
+    "duration",
+)
+SAMPLES_H = ("session", "tstamp", "role", "seq", "freq", "temp_box")
+
+# -----------------------
+# Module global variables
+# -----------------------
+
+# get the root logger
+log = logging.getLogger(__name__.split(".")[-1])
+
+# -------------------
+# Auxiliary functions
+# -------------------
+
+
+def write_csv(path: str, header: Sequence[str], iterable: Iterable[str], delimiter: str = ";"):
+    with open(path, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=delimiter)
+        writer.writerow(header)
+        for row in iterable:
+            writer.writerow(row)
+
+
+def _extract_batch(path: str, conn) -> None:
+    log.info("Extracting from batch_t table.")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT begin_tstamp, end_tstamp, calibrations, email_sent, comment 
+        FROM batch_t 
+        ORDER BY begin_tstamp
+    """)
+    write_csv(path, BATCH_H, cursor)
+
+
+def _extract_config(path: str, conn) -> None:
+    log.info("Extracting from config_t table.")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT section, property AS prop, value
+        FROM config_t 
+        ORDER BY section, prop
+    """)
+    write_csv(path, CONFIG_H, cursor)
+
+
+def _extract_photometer(path: str, conn) -> None:
+    log.info("Extracting from summary_t table for photometer data.")
+    cursor = conn.cursor()
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT DISTINCT name,mac,sensor,model,firmware,filter,plug,box,collector
+        FROM summary_t
+        ORDER BY name
+    """)
+    write_csv(path, PHOTOMETER_H, cursor)
+
+
+def _extract_summary(path: str, conn) -> None:
+    log.info("Extracting from summary_t table for summary calibration data.")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT DISTINCT name,mac,session,role,calibration,calversion,author,nrounds,offset AS zp_offset,
+            upd_flag,prev_zp,zero_point,zero_point_method,freq,freq_method,mag,comment
+        FROM summary_t
+        ORDER BY name
+    """)
+    write_csv(path, SUMMARY_H, cursor)
+
+
+def _extract_rounds(path: str, conn) -> None:
+    log.info("Extracting from rounds_t table.")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT DISTINCT session,round,role,begin_tstamp,end_tstamp,central,freq,stddev,mag,zp_fict,zero_point,nsamples,duration
+        FROM rounds_t
+        ORDER BY session, round, role
+    """)
+    write_csv(path, ROUNDS_H, cursor)
+
+
+def _extract_samples(path: str, conn) -> None:
+    log.info("Extracting from samples_t table.")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT session,tstamp,role,seq,freq,temp_box 
+        FROM samples_t 
+        ORDER BY session, tstamp, role 
+    """)
+    write_csv(path, SAMPLES_H, cursor)
+
+
+# --------------
+# main functions
+# --------------
+
+
+def prs_idir() -> ArgumentParser:
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument(
+        "-i",
+        "----input-dir",
+        type=vdir,
+        default=os.getcwd(),
+        metavar="<Dir>",
+        help="Input CSV directory (default %(default)s)",
+    )
+    return parser
+
+
+def add_args(parser: ArgumentParser) -> None:
+    subparser = parser.add_subparsers(dest="command")
+    subparser.add_parser("config", parents=[prs_idir()], help="Extract config CSV")
+    subparser.add_parser("batch", parents=[prs_idir()], help="Extract batch CSV")
+    subparser.add_parser("photometer", parents=[prs_idir()], help="Extract photometer CSV")
+    subparser.add_parser("summary", parents=[prs_idir()], help="Extract summary CSV")
+    subparser.add_parser("rounds", parents=[prs_idir()], help="Extract rounds CSV")
+    subparser.add_parser("samples", parents=[prs_idir()], help="Extract samples CSV")
+    subparser.add_parser("all", parents=[prs_idir()], help="Extract all CSVs")
+
+
+TABLE = {
+    "config": _extract_config,
+    "batch": _extract_batch,
+    "photometer": _extract_photometer,
+    "summary": _extract_summary,
+    "rounds": _extract_rounds,
+    "samples": _extract_samples,
+}
+
+
+def cli_main(args: Namespace) -> None:
+    connection, _ = open_database(env_var="SOURCE_DATABASE")
+    if args.command not in ("all",):
+        func = TABLE[args.command]
+        path = os.path.join(args.input_dir, args.command + ".csv")
+        func(path, connection)
+        log.info("done.")
+    else:
+        for name in ("config", "batch", "photometer", "summary", "rounds", "samples"):
+            path = os.path.join(args.input_dir, name + ".csv")
+            func = TABLE[name]
+            func(path, connection)
+    connection.close()
+
+
+def main():
+    """The main entry point specified by pyproject.toml"""
+    execute(
+        main_func=cli_main,
+        add_args_func=add_args,
+        name=__name__,
+        version=__version__,
+        description=DESCRIPTION,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/zptess/lib/dbase/model.py
@@ -0,0 +1,385 @@
+# ----------------------------------------------------------------------
+# Copyright (c) 2024 Rafael Gonzalez.
+#
+# See the LICENSE file for details
+# ----------------------------------------------------------------------
+
+
+# --------------------
+# System wide imports
+# -------------------
+
+import sys
+import logging
+
+from typing import Optional, List
+from datetime import datetime
+
+# =====================
+# Third party libraries
+# =====================
+
+
+if sys.version_info[1] < 11:
+    from typing_extensions import Self
+else:
+    from typing import Self
+
+
+from sqlalchemy import (
+    select,
+    Enum,
+    Table,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from lica.sqlalchemy.asyncio.dbase import Model
+from lica.sqlalchemy.view import view
+from lica.asyncio.photometer import Model as PhotModel, Role, Sensor
+
+from .. import CentralTendency, Calibration
+
+# ================
+# Module constants
+# ================
+
+# =======================
+# Module global variables
+# =======================
+
+# get the module logger
+log = logging.getLogger(__name__)
+
+
+def datestr(dt: datetime) -> str:
+    return dt.strftime("%Y-%m-%d %H:%M:%S.%f") if dt is not None else None
+
+
+# =================================
+# Data Model, declarative ORM style
+# =================================
+
+# ---------------------------------------------
+# Additional conveniente types for enumerations
+# ---------------------------------------------
+
+RoleType: Enum = Enum(
+    Role,
+    name="role_type",
+    create_constraint=False,
+    metadata=Model.metadata,
+    validate_strings=True,
+    values_callable=lambda x: [e.name.lower() for e in x],
+)
+
+PhotModelType: Enum = Enum(
+    PhotModel,
+    name="model_type",
+    create_constraint=False,
+    metadata=Model.metadata,
+    validate_strings=True,
+    values_callable=lambda x: [e.value for e in x],
+)
+
+
+SensorType: Enum = Enum(
+    Sensor,
+    name="sensor_type",
+    create_constraint=False,
+    metadata=Model.metadata,
+    validate_strings=True,
+    values_callable=lambda x: [e.value for e in x],
+)
+
+CentralTendencyType: Enum = Enum(
+    CentralTendency,
+    name="central_type",
+    create_constraint=False,
+    metadata=Model.metadata,
+    validate_strings=True,
+    values_callable=lambda x: [e.value for e in x],
+)
+
+CalibrationType: Enum = Enum(
+    Calibration,
+    name="calibration_type",
+    create_constraint=False,
+    metadata=Model.metadata,
+    validate_strings=True,
+    values_callable=lambda x: [e.value for e in x],
+)
+
+# --------
+# Entities
+# --------
+
+
+class Config(Model):
+    __tablename__ = "config_t"
+
+    section: Mapped[str] = mapped_column(String(32), primary_key=True)
+    prop: Mapped[str] = mapped_column("property", String(255), primary_key=True)
+    value: Mapped[str] = mapped_column(String(255))
+
+    def __repr__(self) -> str:
+        return f"Config(section={self.section!r}, prop={self.prop!r}, value={self.value!r})"
+
+
+class Batch(Model):
+    __tablename__ = "batch_t"
+
+    begin_tstamp: Mapped[datetime] = mapped_column(DateTime, primary_key=True)
+    end_tstamp: Mapped[Optional[datetime]] = mapped_column(DateTime)
+    email_sent: Mapped[Optional[bool]]
+    calibrations: Mapped[Optional[int]]
+    comment: Mapped[Optional[str]] = mapped_column(String(255))
+
+    def __repr__(self) -> str:
+        return f"Batch(begin={datestr(self.begin_tstamp)}, end={datestr(self.end_tstamp)}, N={self.calibrations!r}, emailed={self.email_sent!r})"
+
+
+class Photometer(Model):
+    __tablename__ = "photometer_t"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(10))
+    mac: Mapped[str] = mapped_column(String(17))
+    sensor: Mapped[SensorType] = mapped_column(SensorType, default=Sensor.TSL237)
+    model: Mapped[PhotModelType] = mapped_column(PhotModelType)
+    firmware: Mapped[Optional[str]] = mapped_column(String(17))
+    filter: Mapped[Optional[str]] = mapped_column(String(32), default="UV/IR-740")
+    plug: Mapped[Optional[str]] = mapped_column(String(16), default="USB-A")
+    box: Mapped[Optional[str]] = mapped_column(String(16), default="FSH714")
+    collector: Mapped[Optional[str]] = mapped_column(
+        String(16), default="standard"
+    )  #  Collector model
+
+    # This is not a real column, it s meant for the ORM
+    calibrations: Mapped[List["Summary"]] = relationship(back_populates="photometer")
+
+    def __repr__(self) -> str:
+        return f"Photom(id={self.id!r}, name={self.name!r}, mac={self.mac!r})"
+
+    __table_args__ = (
+        UniqueConstraint(
+            name,
+            mac,
+        ),
+        {},
+    )
+
+
+class Summary(Model):
+    __tablename__ = "summary_t"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    phot_id: Mapped[int] = mapped_column(ForeignKey("photometer_t.id"), index=True)
+    session: Mapped[datetime] = mapped_column(
+        DateTime
+    )  # calibration session identifier
+    role: Mapped[RoleType] = mapped_column(RoleType)
+    calibration: Mapped[CalibrationType] = mapped_column(CalibrationType, nullable=True)
+    calversion: Mapped[Optional[str]] = mapped_column(
+        String(64)
+    )  # calibration software version
+    author: Mapped[Optional[str]]  # who run the calibration
+    nrounds: Mapped[Optional[int]]  # Number of rounds passed
+    zp_offset: Mapped[
+        Optional[float]
+    ]  # Additional offset that was summed to the computed zero_point
+    upd_flag: Mapped[
+        Optional[bool]
+    ]  # 1 => TESS-W ZP was updated, 0 => TESS-W ZP was not updated
+    prev_zp: Mapped[Optional[float]]
+    prev_freq_offset: Mapped[Optional[float]]
+    zero_point: Mapped[Optional[float]]  #  calibrated zero point
+    zero_point_method: Mapped[CentralTendencyType] = mapped_column(
+        CentralTendencyType, nullable=True
+    )
+    freq: Mapped[Optional[float]]  # final chosen frequency
+    freq_method: Mapped[CentralTendencyType] = mapped_column(
+        CentralTendencyType, nullable=True
+    )
+    mag: Mapped[Optional[float]]
+    comment: Mapped[Optional[str]] = mapped_column(
+        String(512)
+    )  #  Additional comment for the calibration process
+
+    # This is not a real column, it s meant for the ORM
+    photometer: Mapped["Photometer"] = relationship(back_populates="calibrations")
+    # This is not a real column, it s meant for the ORM
+    rounds: Mapped[List["Round"]] = relationship(back_populates="summary")
+    # This is not a real column, it s meant for the ORM
+    samples: Mapped[List["Sample"]] = relationship(back_populates="summary")
+
+    def __repr__(self) -> str:
+        return f"Summary(session={datestr(self.session)}, role={self.role!r}, phot_id={self.phot_id!r})"
+
+    __table_args__ = (UniqueConstraint(session, role), {})
+
+
+# Samples per round
+# Due to the sliding window collect process, a sample may belong to several rounds
+# This part is not part of the ORM, as it uses the basic Table API
+SamplesRounds = Table(
+    "samples_rounds_t",
+    Model.metadata,
+    Column("round_id", ForeignKey("rounds_t.id"), nullable=False, primary_key=True),
+    Column("sample_id", ForeignKey("samples_t.id"), nullable=False, primary_key=True),
+)
+
+
+class Round(Model):
+    __tablename__ = "rounds_t"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    summ_id: Mapped[int] = mapped_column(ForeignKey("summary_t.id"), index=True)
+    seq: Mapped[int] = mapped_column("round", Integer)  # Round number form 1..NRounds
+    role: Mapped[RoleType] = mapped_column(RoleType)
+    # session:    Mapped[datetime] = mapped_column(DateTime)
+    freq: Mapped[Optional[float]]  # Average of Median method
+    central: Mapped[CentralTendencyType] = mapped_column(
+        CentralTendencyType, nullable=True
+    )
+    stddev: Mapped[Optional[float]]  # Standard deviation for frequency central estimate
+    mag: Mapped[
+        Optional[float]
+    ]  # magnitiude corresponding to central frequency and summing ficticious zero point
+    zp_fict: Mapped[
+        Optional[float]
+    ]  # Ficticious ZP to estimate instrumental magnitudes (=20.50)
+    zero_point: Mapped[
+        Optional[float]
+    ]  # Estimated Zero Point for this round ('test' photometer round only, else NULL)
+    nsamples: Mapped[Optional[int]]  # Number of samples for this round
+    duration: Mapped[Optional[float]]  # Approximate duration, in seconds
+    begin_tstamp: Mapped[Optional[datetime]] = mapped_column(DateTime)
+    end_tstamp: Mapped[Optional[datetime]] = mapped_column(DateTime)
+
+    # This is not a real column, it s meant for the ORM
+    summary: Mapped["Summary"] = relationship(back_populates="rounds")
+    # samples per round. Shoudl match the window size
+    # This is not a real column, it s meant for the ORM
+    samples: Mapped[List["Sample"]] = relationship(
+        secondary=SamplesRounds, back_populates="rounds"
+    )
+
+    def __repr__(self) -> str:
+        return f"Round(id={self.id!r}, #{self.seq!r} [{self.nsamples!r}] {self.role!r}, zp={self.zero_point} f={self.freq}, m={self.mag:.2f}@{self.zp_fict} Ts={datestr(self.begin_tstamp)}, Te={datestr(self.end_tstamp)})"
+
+    __table_args__ = (UniqueConstraint(summ_id, seq, role), {})
+
+
+class Sample(Model):
+    __tablename__ = "samples_t"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    summ_id: Mapped[int] = mapped_column(ForeignKey("summary_t.id"), index=True)
+    tstamp: Mapped[datetime] = mapped_column(DateTime)
+    role: Mapped[RoleType] = mapped_column(RoleType)
+    seq: Mapped[Optional[int]]
+    freq: Mapped[float]
+    temp_box: Mapped[Optional[float]]
+
+    # rounds per sample (at least 1...)
+    # This is not a real column, it s meant for the ORM
+    rounds: Mapped[List["Round"]] = relationship(
+        secondary=SamplesRounds, back_populates="samples"
+    )
+
+    # This is not a real column, it s meant for the ORM
+    summary: Mapped["Summary"] = relationship(back_populates="samples")
+
+    def __repr__(self) -> str:
+        return f"Sample(id={self.id!r}, role={self.role!r} freq={self.freq!r},  seq={self.seq!r})"
+
+    def __lt__(self, other: Self) -> bool:
+        return self.tstamp < other.tstamp
+
+    def __le__(self, other: Self) -> bool:
+        return self.tstamp <= other.tstamp
+
+    def __eq__(self, other: Self) -> bool:
+        return self.tstamp == other.tstamp
+
+    def __ne__(self, other: Self) -> bool:
+        return self.tstamp != other.tstamp
+
+    def __gt__(self, other: Self) -> bool:
+        return self.tstamp > other.tstamp
+
+    def __ge__(self, other: Self) -> bool:
+        return self.tstamp >= other.tstamp
+
+    __table_args__ = (UniqueConstraint(tstamp, role), {})
+
+
+# Create the view for barebones SQL statements from console
+summary_view = view(
+    name="summary_v",
+    metadata=Model.metadata,
+    selectable=select(
+        Summary.__table__.c.id.label("id"),
+        Photometer.__table__.c.name.label("name"),
+        Photometer.__table__.c.mac.label("mac"),
+        Summary.__table__.c.session.label("session"),
+        Summary.__table__.c.role.label("role"),
+        Summary.__table__.c.nrounds.label("nrounds"),
+        Summary.__table__.c.upd_flag.label("upd_flag"),
+        Summary.__table__.c.zero_point.label("zero_point"),
+        Summary.__table__.c.calibration.label("calibration"),
+        Summary.__table__.c.prev_zp.label("prev_zp"),
+        Summary.__table__.c.freq.label("freq"),
+        Summary.__table__.c.mag.label("mag"),
+        Summary.__table__.c.prev_freq_offset.label("prev_freq_offset"),
+        Summary.__table__.c.zero_point_method.label("zero_point_method"),
+        Summary.__table__.c.freq_method.label("freq_method"),
+        Summary.__table__.c.calversion.label("calversion"),
+        Summary.__table__.c.zp_offset.label("zp_offset"),
+        Summary.__table__.c.comment.label("comment"),
+    ).select_from(Summary.__table__.join(Photometer.__table__)),
+)
+
+# Another view for debugging data
+rounds_view = view(
+    name="rounds_v",
+    metadata=Model.metadata,
+    selectable=select(
+        Round.__table__.c.id.label("id"),
+        Photometer.__table__.c.name.label("name"),
+        Photometer.__table__.c.mac.label("mac"),
+        Photometer.__table__.c.model.label("model"),
+        Summary.__table__.c.session.label("session"),
+        Round.__table__.c.round.label("round"),
+        Round.__table__.c.role.label("role"),
+        Round.__table__.c.freq.label("freq"),
+        Round.__table__.c.central.label("central"),
+        Round.__table__.c.stddev.label("stddev"),
+        Round.__table__.c.mag.label("mag"),
+        Round.__table__.c.zp_fict.label("zp_fict"),
+        Round.__table__.c.zero_point.label("zero_point"),
+        Round.__table__.c.nsamples.label("nsamples"),
+        Round.__table__.c.duration.label("duration"),
+        Round.__table__.c.begin_tstamp.label("begin_tstamp"),
+        Round.__table__.c.end_tstamp.label("end_tstamp"),
+        Summary.__table__.c.upd_flag.label("upd_flag"),
+        Summary.__table__.c.nrounds.label("nrounds"),
+        Summary.__table__.c.freq.label("mean_freq"),
+        Summary.__table__.c.freq_method.label("freq_method"),
+    )
+    .select_from(Round.__table__.join(Summary.__table__))
+    .join(Photometer.__table__),
+)
+
+
+class SummaryView(Model):
+    __table__ = summary_view
+
+    def __repr__(self) -> str:
+        return f"SummaryView(name={self.name}, mac={self.mac}, session={datestr(self.session)}, role={self.role!r}, nrounds={self.nrounds!r}, zp={self.zero_point!r}, calib={self.calibration!r}, freq={self.freq!r})"
