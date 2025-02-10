9a6a55e10176da396d0dcc7b8803fed106dca87d
Rafael Gonzalez <astrorafael@gmail.com>
2025-02-10 11:32:15 +0100

    starting db things

--- src/zptess/lib/photometer/persistent.py
+++ src/zptess/lib/photometer/persistent.py
@@ -11,24 +11,25 @@ from __future__ import annotations
 
 import logging
 import asyncio
-from abc import ABC, abstractmethod
 
-from typing import Any, Mapping
+from typing import Any, Mapping, Dict
 
 
 # ---------------------------
 # Third-party library imports
 # ----------------------------
 
+from sqlalchemy import select
 from pubsub import pub
 from lica.asyncio.photometer import Role
-
+from lica.sqlalchemy.asyncio.dbase import AsyncSession
 # --------------
 # local imports
 # -------------
 
 from .volatile import Controller as VolatileCalibrator
 from .types import Event
+from ..dbase.model import Photometer
 
 # ----------------
 # Module constants
@@ -63,19 +64,9 @@ class Controller(VolatileCalibrator):
     ):
         super().__init__(ref_params, test_params, common_params)
         self.db_queue = asyncio.Queue()
-        self._state = None
-
-    def transition_to(self, state: State):
-        """
-        The Context allows changing the State object at runtime.
-        """
-        log.info("Transition to %s", type(state).__name__)
-        self._state = state
-        self._state.controller = self
 
     async def init(self) -> None:
         await super().init()
-        self.transition_to(StartState())
         self.db_task = asyncio.create_task(self.db_writer())
 
     async def db_writer(self) -> None:
@@ -84,13 +75,14 @@ class Controller(VolatileCalibrator):
             msg = await self.db_queue.get()
             event = msg["event"]
             if event == Event.CAL_START:
-                self._state.handle_start()
+                pass
             elif event == Event.ROUND:
-                self._state.handle_round(msg["info"], msg["samples"])
+                self.temp_round_info = msg["info"]
+                self.temp_round_samples = msg["samples"]
             elif event == Event.SUMMARY:
-                self._state.handle_summary(msg["info"])
+                self.temp_summary = msg["info"]
             else:
-                await self._state.handle_end()
+                await self.do_persist()
         log.info("Database listener ends here")
 
     def on_calib_start(self) -> None:
@@ -122,100 +114,27 @@ class Controller(VolatileCalibrator):
         msg = {"event": Event.SUMMARY, "info": summary_info}
         self.db_queue.put_nowait(msg)
 
+    async def do_photometer(self, session: AsyncSession) -> [Dict[Role,Photometer]]:
+        phot = dict()
+        await asyncio.sleep(1)
+        return phot
+        for role in self.roles():
+            log.info(self.phot_info[role])
+            name = self.phot_info[role]["name"]
+            mac = self.phot_info[role]["mac"]
+            q = select(Photometer).where(Photometer.mac == mac, Photometer.name == name)
+            phot[role] = (await session.scalars(q)).one_or_none()
+            if phot[role] is None:
+                col = dict()
+                for key in ("sensor", "firmware", "filter", "collector", "comment"):
+                    col[key] = None if not self.phot_info[role][key] else self.phot_info[role][key]
+                log.info("Creating ned DB Photometer for %s, %s", name, mac)
+                phot[role] = Photometer(**col)
+                session.add(phot[role])
+        return phot
 
-class State(ABC):
-    """
-    The base State class declares methods that all Concrete State should
-    implement and also provides a backreference to the Context object,
-    associated with the State. This backreference can be used by States to
-    transition the Context to another State.
-    """
-
-    _ctrl = None
-
-    @property
-    def controller(self) -> Controller:
-        return self._ctrl
-
-    @controller.setter
-    def controller(self, ctrl: Controller) -> None:
-        self._ctrl = ctrl
-
-    @abstractmethod
-    def handle_start(self) -> None:
-        pass
-
-    @abstractmethod
-    async def handle_end(self) -> None:
-        pass
-
-    @abstractmethod
-    def handle_round(self, round_info: Mapping[str, Any], samples: Mapping[str, Any]) -> None:
-        pass
-
-    @abstractmethod
-    def handle_summary(self, round_info: Mapping[str, Any]) -> None:
-        pass
-
-
-class StartState(State):
-    def handle_start(self) -> None:
-        log.info("%s.%s()", self.__class__.__name__, self.handle_start.__name__)
-        self.controller.transition_to(RoundState())
-
-    async def handle_end(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_end.__name__)
-
-    def handle_round(self, round_info: Mapping[str, Any], samples: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_round.__name__)
-
-    def handle_summary(self, round_info: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_summary.__name__)
-
-
-class RoundState(State):
-    def handle_start(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_start.__name__)
-
-    async def handle_end(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_end.__name__)
-
-    def handle_round(self, round_info: Mapping[str, Any], samples: Mapping[str, Any]) -> None:
-        log.info("%s.%s()", self.__class__.__name__, self.handle_round.__name__)
-        if round_info["current"] == self.controller.nrounds:
-            self.controller.transition_to(SummaryState())
-
-    def handle_summary(self, round_info: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_summary.__name__)
-
-
-class SummaryState(State):
-    def handle_start(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_start.__name__)
-
-    async def handle_end(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_end.__name__)
-
-    def handle_round(self, round_info: Mapping[str, Any], samples: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_round.__name__)
-
-    def handle_summary(self, round_info: Mapping[str, Any]) -> None:
-        log.info("%s.%s()", self.__class__.__name__, self.handle_summary.__name__)
-        self.controller.transition_to(EndState())
-
-
-class EndState(State):
-    def handle_start(self) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_start.__name__)
-
-    def handle_round(self, round_info: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_round.__name__)
-
-    def handle_summary(self, round_info: Mapping[str, Any], samples: Mapping[str, Any]) -> None:
-        log.warn("Ignoring %s.%s", self.__class__.__name__, self.handle_summary.__name__)
-
-    async def handle_end(self) -> None:
-        log.info("%s.%s()", self.__class__.__name__, self.handle_end.__name__)
-        await asyncio.sleep(4)
-        self.controller.calibrating = False
-        log.info("Ending Database Calibration Process")
+    async def do_persist(self):
+        async with self.Session() as session:
+            async with session.begin():
+                photometers = await self.do_photometer(session)
+                log.info(" ============================================== JOOODERR %s",photometers)
